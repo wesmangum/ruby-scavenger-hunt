006aa8dfb8a2ea7c155cc4ae2ec4d45703f0187b
wesmangum <wesmangum24@gmail.com>
2014-07-22 12:11:08 -0500

    plathlength task completed

--- lib/vector.rb
+++ lib/vector.rb
@@ -1,15 +1,16 @@
 class Point < Vector
-  attr_accessor :point
-
-  def initialize(point_x, point_y)
-    @point = Vector[point_x, point_y]
-  end
-
-  def distance_to(x2, y2)
-    point = Vector[x2, y2]
+  def distance(point)
     x_part = (point[0] - self.point[0]) ** 2
     y_part = (point[1] - self.point[1]) ** 2
     dist = Math.sqrt(x_part + y_part)
+    return dist
+  end
+
+  def self.pathlength (points*)
+    dist = 0
+    points.each do |point, next_point|
+      dist += point.distance(next_point)
+    end
     dist
   end
 end
